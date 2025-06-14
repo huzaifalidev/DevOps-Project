704921e2a54a3af46279945117e6c176f80aac0c
huzaifa-Ali-Khan <huzaifalikhan48@gmail.com>
2025-06-14 16:28:20 +0500

    Fix: Update public IP resource name and add SKU configuration

--- terraform/main.tf
+++ terraform/main.tf
@@ -64,11 +64,12 @@ resource "azurerm_network_interface" "nic" {
   }
 }
 
-resource "azurerm_public_ip" "public_ip" {
-  name                = "devops-project-pip"
+resource "azurerm_public_ip" "pip" {
+  name                = "myPublicIP"
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   allocation_method   = "Dynamic"
+  sku                = "Basic"
 }
 
 resource "azurerm_linux_virtual_machine" "vm" {
